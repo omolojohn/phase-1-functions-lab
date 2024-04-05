4425efb30f5c9237b2ead2a6955b2bd4d24adc5e
omolo-j <omoloj135@gmail.com>
2024-04-05 22:31:30 +0300

    functions lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+    return Math.abs(location - 42);
+}
+function distanceFromHqInFeet(location) {
+    return distanceFromHqInBlocks(location) * 264;
+}
+function distanceTravelledInFeet(startBlock, endBlock) {
+    const blocksTravelled = Math.abs(endBlock - startBlock);
+    return blocksTravelled * 264;
+}
+function calculatesFarePrice(startBlock, endBlock) {
+    const distance = distanceTravelledInFeet(startBlock, endBlock);
+    
+    if (distance <= 400) {
+        return 0;
+    } else if (distance > 400 && distance <= 2000) {
+        return (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
